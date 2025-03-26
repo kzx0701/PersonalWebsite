42cb936277b9dfd878110f8619c9b0faf3d8a1de
unknown <839711518@qq.com>
2025-03-26 22:54:06 +0800

    新增登录罗技

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,3 +1,17 @@
-const login = (userName : string,password : string) => {
-    
-}
+// 登录
+export const login = (userName: string, password: string) => {
+  if (verify(userName, password)) {
+    console.log("验证成功");
+  } else {
+    console.log("验证失败");
+  }
+};
+
+// 验证
+const verify = (userName: string, password: string) => {
+  if (!userName || !password) {
+    console.error("用户名和密码不能为空");
+    return false;
+  }
+  return true;
+};
