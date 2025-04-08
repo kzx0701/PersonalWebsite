6fc48b08347ed07dd239eb782f1bc097897d0c76
KouziXuan <839711518@qq.com>
2025-04-08 09:39:04 +0800

    解决bug

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,28 @@
 <template>
-    <div>这是首页</div>
+  <div class="home_frame">
+    <div>欢迎回来</div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home_frame {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: radial-gradient(circle at 10% 20%, rgba(33, 33, 33, 1) 0%, rgba(14, 14, 26, 1) 90%);
+
+  // 添加一些微妙的粒子效果
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: radial-gradient(circle at 20% 30%, rgba(31, 215, 232, 0.05) 1px, transparent 1px),
+      radial-gradient(circle at 80% 70%, rgba(167, 139, 250, 0.05) 1px, transparent 1px);
+    background-size: 30px 30px;
+    opacity: 0.8;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,14 +15,15 @@ router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore();
   await authStore.loadSession();
   // 未登录跳转到登录页面
-  if (!authStore.session.access_token) {
+  if (to.path !== "/login" && !authStore.session.access_token) {
     return next("/login");
   }
   // 已登录跳转到首页
-  if (to.path === "/login") {
+  if (to.path === "/login" && authStore.session.access_token) {
     console.log("登录页面", to.path);
     return next("/");
   }
+  // 其他情况正常跳转
   next();
 });
 
